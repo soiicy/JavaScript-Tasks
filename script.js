141a9cf6071c6c3bf68409170e9b4722fbce83b4
soicy <soicyinyourmind@gmail.com>
2022-11-24 22:44:04 +0300

    Simple Interface Task

---
+++ script.js
@@ -0,0 +1,7 @@
+const currentUserName   = prompt ('What is your first name?'),
+    currentUserSur      = prompt ('What is your surname?'),
+    currentUserAge      = prompt ('How old are you?');
+
+alert (`Your name is ${currentUserName} ${currentUserSur} and you are ${currentUserAge} years old.`);
+
+
