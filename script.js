0e246266d74f8dbdf3d85c645f09aa1970520b25
soicy <soicyinyourmind@gmail.com>
2022-12-09 12:59:28 +0300

    upd

--- script.js
+++
@@ -1,7 +0,0 @@
-const currentUserName   = prompt ('What is your first name?'),
-    currentUserSur      = prompt ('What is your surname?'),
-    currentUserAge      = prompt ('How old are you?');
-
-alert (`Your name is ${currentUserName} ${currentUserSur} and you are ${currentUserAge} years old.`);
-
-
