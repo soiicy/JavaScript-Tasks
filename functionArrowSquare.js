1a3ecd7353462d3dc8ebee2d65be6838b00896a8
soicy <soicyinyourmind@gmail.com>
2023-01-06 15:18:53 +0300

    Implemented square function

---
+++ functionArrowSquare.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const makeSquare = (size) => {
+    const square = '■';
+    const inside = ' ';
+    let startLine = '';
+
+    for (let line = 1; line <= size; line++) {
+        if (line === 1 || line === size) {
+            startLine += square.repeat(size) + '\n';
+        } else {
+            startLine += square + (inside.repeat(size - 2)) + square + '\n';
+        }
+    }
+    return startLine;
+}
+
+console.log(makeSquare(3));
---
+++ readme.md
@@ -0,0 +1,8 @@
+##Square Function Task
+
+Implement square function(all functions types) which will take one argument(square size) and print in console empty inside square 
+
+#Example
+square(8)
+
+![alt example](blob:https://web.telegram.org/71e1cde7-8df6-43d9-b39a-896103832ae7)
