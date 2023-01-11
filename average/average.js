2778e114fbe9719bc86bea322dfa96a981b1edc5
soicy <soicyinyourmind@gmail.com>
2023-01-11 16:20:49 +0300

    Reworked all solutions using function

--- average/average.js
+++ average/average.js
@@ -1,17 +1,25 @@
-const xUser = Math.floor((Math.random() * 100) + 1);
-const yUser = Math.floor((Math.random() * 100) + 1);
-const zUser = Math.floor((Math.random() * 100) + 1); //🤨
 
-console.log(xUser, yUser, zUser);
+const findAverage = () => {
+    if (calcAverage() >= 60) {
+        console.log('Average age of users more than 60');
+    }
 
-const averageAge = (xUser + yUser + zUser) / 3;
-
-console.log (averageAge);
-
-if(averageAge >= 60){
-    console.log('Average age of users more than 60');
- }
-
-else {
-    console.log('Average age of users less than 60');
+    else {
+        console.log('Average age of users less than 60');
+    }
 }
+
+const calcAverage = () => {
+    const xUser = Math.floor((Math.random() * 100) + 1);
+    const yUser = Math.floor((Math.random() * 100) + 1);
+    const zUser = Math.floor((Math.random() * 100) + 1); 
+
+    const averageAge = (xUser + yUser + zUser) / 3;
+
+    console.log(xUser, yUser, zUser);
+    console.log(averageAge);
+
+    return averageAge;
+} 
+
+findAverage();
--- logical operators/logical_operators.js
+++ logical operators/logical_operators.js
@@ -1,21 +1,26 @@
-let xUser = prompt("How old are you?");
-let yUser = prompt("How old are you?");
-let zUser = prompt("How old are you?");
 
-xUser = Number(xUser);
-yUser = Number(yUser);
-zUser = Number(zUser);
+function calcAverage() {
+    let xUser = +prompt("How old are you?");
+    let yUser = +prompt("How old are you?");
+    let zUser = +prompt("How old are you?");
 
-if (!(isNaN(xUser)) && !(isNaN(yUser)) && !(isNaN(zUser))) {
+    if (!(isNaN(xUser)) && !(isNaN(yUser)) && !(isNaN(zUser))) {
+        const averageAge = (xUser + yUser + zUser) / 3;
+        findMoreLess(averageAge);
 
-    const averageAge = (xUser + yUser + zUser) / 3;
+    } else {
+        console.log('I think you entered something wrong');
+    }
+}
 
+function findMoreLess(averageAge) {
     if (averageAge >= 60) {
         console.log('Average age of users more than 60');
+
     } else if (averageAge < 60) {
         console.log('Average age of users less than 60');
-    }
 
-} else {
-    console.log('I think you entered something wrong');
+    }
 }
+
+calcAverage();
